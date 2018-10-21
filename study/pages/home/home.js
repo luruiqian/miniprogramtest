5eb7038dde549a6d983a621017a8784d0b4daaf4
luruiqian <luruiqian@gomeplus.com>
2018-10-21 19:50:17 +0800

    首次创建

---
+++ study/pages/home/home.js
@@ -0,0 +1,76 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    var that = this;
+    wx.request({
+      url:"https://www.easy-mock.com/mock/5bcae41c0616ca41e7001877/555/555",
+      method:"GET",
+      success:function(res){
+        that.setData({//将数据发送到data中
+        restaurant:res.data.data.restaurant,
+        location:wx.getStorageSync('location')
+        })
+      }
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
